c81eb71b5dbacb958cc8741493fac154a2e4c6e7
unknown <olysiuk01@gmail.com>
2020-09-26 15:34:11 +0300

    toolbar layout

---
+++ src/components/search-field.vue
@@ -0,0 +1,27 @@
+<template >
+   <v-form >
+    <v-container>
+      <v-row  >
+
+        <v-col cols="12" xs='12'>
+          <v-text-field
+            label="Solo"
+            single-line
+            solo
+          ></v-text-field>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'search-field'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/toolbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="65px"
+    tile
+  >
+    <v-toolbar class="indigo accent-1">
+      <v-app-bar-nav-icon  @click='drawer= !drawer' ></v-app-bar-nav-icon>
+
+      <v-toolbar-title >Github API</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="indigo accent-1" temporary overlay-opacity="0.5">
+        <v-list>
+            <v-list-item-group v-model="item">
+                <v-list-item v-for='( item, index ) in items' :key='index' router :to='item.route'>
+                    <v-list-item-icon >
+                        <v-icon v-text="item.icon"></v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'toolbar',
+  data () {
+    return {
+      drawer: false,
+      item: 1,
+      items: [
+        { text: 'Home', icon: 'mdi-home', route: '/' },
+        { text: 'About', icon: 'mdi-information', route: 'about' }
+      ]
+    }
+  }
+}
+</script>
