73be3298ad42bf6bcf717cc3450a34a08d6439e6
unknown <olysiuk01@gmail.com>
2020-09-26 20:38:17 +0300

    pagination component(layout)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,22 @@
           <div class="cards">
            <cards></cards>
           </div>
+          <div class="pagination">
+            <pagination></pagination>
+          </div>
         </v-container>
   </div>
 </template>
 <script>
 import searchField from '../components/search-field'
 import cards from '../components/cards'
+import pagination from '../components/pagination'
 export default {
   name: 'Home',
   components: {
     'search-field': searchField,
-    cards
+    cards,
+    pagination
   }
 }
 </script>
